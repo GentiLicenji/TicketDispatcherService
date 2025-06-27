767b6c8d162e9defc2cf6cae6326d305c2af9c14
Gentian Licenji <gentian.licenji@gmail.com>
2025-06-27 18:05:41 +0200

    PLS-2: Generated project scaffolding using Sprint init integration with IntelliJ.

---
+++ src/main/java/com/pleased/ticket/dispatcher/server/TicketDispatcherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pleased.ticket.dispatcher.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketDispatcherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketDispatcherServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pleased/ticket/dispatcher/server/TicketDispatcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pleased.ticket.dispatcher.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketDispatcherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
