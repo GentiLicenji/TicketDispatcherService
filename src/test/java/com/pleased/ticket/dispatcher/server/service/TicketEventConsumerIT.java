74c6d82d2bd13d98a73cfa64fe4de24bbf7c93c5
Gentian Licenji <gentian.licenji@gmail.com>
2025-07-07 04:53:09 +0200

    PLS-3: Fixed and tested successfully the kafka consumer service with the new reactive consumers.

--- src/test/java/com/pleased/ticket/dispatcher/server/service/TicketEventConsumerIT.java
+++ src/test/java/com/pleased/ticket/dispatcher/server/service/TicketEventConsumerIT.java
@@ -1,5 +1,6 @@
 package com.pleased.ticket.dispatcher.server.service;
 
+import com.pleased.ticket.dispatcher.server.config.KafkaTopicConfig;
 import com.pleased.ticket.dispatcher.server.exception.EntityNotFoundException;
 import com.pleased.ticket.dispatcher.server.model.api.TicketStatusEnum;
 import com.pleased.ticket.dispatcher.server.model.dto.ProjectEntity;
@@ -8,20 +9,21 @@ import com.pleased.ticket.dispatcher.server.model.dto.UserEntity;
 import com.pleased.ticket.dispatcher.server.model.events.TicketAssigned;
 import com.pleased.ticket.dispatcher.server.model.events.TicketCreated;
 import com.pleased.ticket.dispatcher.server.model.events.TicketStatusUpdated;
+import com.pleased.ticket.dispatcher.server.model.rest.TicketResponse;
 import com.pleased.ticket.dispatcher.server.repository.ProjectRepository;
 import com.pleased.ticket.dispatcher.server.repository.TicketRepository;
 import com.pleased.ticket.dispatcher.server.repository.UserRepository;
 import com.pleased.ticket.dispatcher.server.util.mapper.UUIDConverter;
-import org.junit.jupiter.api.AfterEach;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.test.StepVerifier;
 
+import java.nio.ByteBuffer;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -38,8 +40,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  *     <li>{@link com.pleased.ticket.dispatcher.server.model.events.TicketStatusUpdated}</li>
  * </ul>
  * <p>
- * Uses an in-memory H2 database with reactive repositories. Each test prepares
- * its own data and verifies persistence or failure scenarios using {@link StepVerifier}.
+ * Uses an in-memory H2 database with reactive repositories. All tests have the same
+ * data setup and verifies persistence or failure scenarios using {@link StepVerifier}.
  * <p>
  * Scenarios covered:
  * <ul>
@@ -49,11 +51,11 @@ import static org.assertj.core.api.Assertions.assertThat;
  *     <li>Handles not-found cases by throwing {@link EntityNotFoundException}</li>
  * </ul>
  * <p>
- * Profile: {@code test}, DB is reset between each test run.
+ * Profile: {@code test}
  */
 @SpringBootTest
 @ActiveProfiles("test")
-@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TicketEventConsumerIT {
 
     @Autowired
@@ -84,7 +86,7 @@ public class TicketEventConsumerIT {
     /**
      * Create the required user,assignee and project before each test run.
      */
-    @BeforeEach
+    @BeforeAll
     void setUpEach() {
         //Clear db entries so we don't attempt double insertion.
         ticketRepository.deleteAll().block();
@@ -125,20 +127,6 @@ public class TicketEventConsumerIT {
         ticketRepository.save(existingTicket).block();
     }
 
-    /**
-     * Resets data for each test call.
-     * <p>
-     * Note: even though H2 is an in memory db, and it shouldn't persist data.
-     * Spring test stores the same context between all test calls.
-     */
-    @AfterEach
-    void tearDownEach() {
-        //Clear db entries so we don't attempt double insertion.
-        ticketRepository.deleteAll().block();
-        userRepository.deleteAll().block();
-        projectRepository.deleteAll().block();
-    }
-
     @Test
     void handleTicketCreated_ShouldCreateTicketInDatabase() {
 
@@ -147,18 +135,24 @@ public class TicketEventConsumerIT {
 
         // Create a ticket event
         TicketCreated event = TicketCreated.newBuilder()
-                .setTicketId(UUIDConverter.uuidToBytes(ticketId))
+                .setTicketId(UUIDConverter.uuidToBytes(newTicket))
                 .setSubject("Test Ticket")
                 .setDescription("This is a test ticket")
                 .setUserId(UUIDConverter.uuidToBytes(userId))
                 .setProjectId(UUIDConverter.uuidToBytes(projectId))
-//                .setCorrelationId(correlationId)
                 .setEventId(UUIDConverter.uuidToBytes(UUID.randomUUID()))
                 .setCreatedAt(OffsetDateTime.now().toInstant())
                 .build();
 
+        ConsumerRecord<ByteBuffer, TicketCreated> record = new ConsumerRecord<>(
+                KafkaTopicConfig.TICKET_CREATE_TOPIC,
+                0,
+                0L,
+                UUIDConverter.uuidToBytes(newTicket),
+                event
+        );
         // Act
-        StepVerifier.create(ticketEventConsumer.handleTicketCreated(event, "ticket-create.v1", 0, 0))
+        StepVerifier.create(ticketEventConsumer.handleTicketCreated(record))
                 .verifyComplete();
 
         // Assert - Query real database
@@ -169,7 +163,7 @@ public class TicketEventConsumerIT {
                     assertThat(savedTicket.getDescription()).isEqualTo("This is a test ticket");
                     assertThat(savedTicket.getUserId()).isEqualTo(userId);
                     assertThat(savedTicket.getProjectId()).isEqualTo(projectId);
-                    assertThat(savedTicket.getStatus()).isEqualTo(TicketStatusEnum.OPEN.toString());
+                    assertThat(savedTicket.getStatus()).isEqualTo(TicketResponse.StatusEnum.OPEN.toString());
                     assertThat(savedTicket.getCreatedAt()).isNotNull();
                     assertThat(savedTicket.getUpdatedAt()).isNull();
                     assertThat(savedTicket.getAssigneeId()).isNull();
@@ -186,13 +180,19 @@ public class TicketEventConsumerIT {
         TicketAssigned event = TicketAssigned.newBuilder()
                 .setTicketId(UUIDConverter.uuidToBytes(ticketId))
                 .setAssigneeId(UUIDConverter.uuidToBytes(assigneeId))
-//                .correlationId(UUID.randomUUID())
                 .setEventId(UUIDConverter.uuidToBytes(UUID.randomUUID()))
                 .setAssignedAt(now.toInstant())
                 .build();
 
+        ConsumerRecord<ByteBuffer, TicketAssigned> record = new ConsumerRecord<>(
+                KafkaTopicConfig.TICKET_ASSIGNMENTS_TOPIC,
+                0,
+                0L,
+                UUIDConverter.uuidToBytes(ticketId),
+                event
+        );
         // Act
-        StepVerifier.create(ticketEventConsumer.handleTicketAssigned(event, "ticket-assignments.v1", 0, 0))
+        StepVerifier.create(ticketEventConsumer.handleTicketAssigned(record))
                 .verifyComplete();
 
         // Assert - Query real database
@@ -218,13 +218,19 @@ public class TicketEventConsumerIT {
         TicketStatusUpdated event = TicketStatusUpdated.newBuilder()
                 .setTicketId(UUIDConverter.uuidToBytes(ticketId))
                 .setStatus(newStatus)
-//                .correlationId(UUID.randomUUID())
                 .setEventId(UUIDConverter.uuidToBytes(UUID.randomUUID()))
                 .setUpdatedAt(now.toInstant())
                 .build();
 
+        ConsumerRecord<ByteBuffer, TicketStatusUpdated> record = new ConsumerRecord<>(
+                KafkaTopicConfig.TICKET_UPDATES_TOPIC,
+                0,
+                0L,
+                UUIDConverter.uuidToBytes(ticketId),
+                event
+        );
         // Act
-        StepVerifier.create(ticketEventConsumer.handleTicketStatusUpdated(event, "ticket-updates.v1", 0, 0))
+        StepVerifier.create(ticketEventConsumer.handleTicketStatusUpdated(record))
                 .verifyComplete();
 
         // Assert - Query real database
@@ -249,13 +255,19 @@ public class TicketEventConsumerIT {
         TicketAssigned event = TicketAssigned.newBuilder()
                 .setTicketId(UUIDConverter.uuidToBytes(ticketId))
                 .setAssigneeId(UUIDConverter.uuidToBytes(assigneeId))
-//                .correlationId(UUID.randomUUID())
                 .setEventId(UUIDConverter.uuidToBytes(UUID.randomUUID()))
                 .setAssignedAt(OffsetDateTime.now().toInstant())
                 .build();
+        ConsumerRecord<ByteBuffer, TicketAssigned> record = new ConsumerRecord<>(
+                KafkaTopicConfig.TICKET_ASSIGNMENTS_TOPIC,
+                0,
+                0L,
+                UUIDConverter.uuidToBytes(ticketId),
+                event
+        );
 
         // Act & Assert - Use StepVerifier to test reactive error handling
-        StepVerifier.create(ticketEventConsumer.handleTicketAssigned(event, "ticket-assignments.v1", 0, 0))
+        StepVerifier.create(ticketEventConsumer.handleTicketAssigned(record))
                 .expectErrorMatches(throwable ->
                         throwable instanceof EntityNotFoundException &&
                                 throwable.getMessage().equals("Ticket not found: " + ticketId))
@@ -275,13 +287,19 @@ public class TicketEventConsumerIT {
         TicketStatusUpdated event = TicketStatusUpdated.newBuilder()
                 .setTicketId(UUIDConverter.uuidToBytes(ticketId))
                 .setStatus(newStatus)
-//                .correlationId(UUID.randomUUID())
                 .setEventId(UUIDConverter.uuidToBytes(UUID.randomUUID()))
                 .setUpdatedAt(OffsetDateTime.now().toInstant())
                 .build();
+        ConsumerRecord<ByteBuffer, TicketStatusUpdated> record = new ConsumerRecord<>(
+                KafkaTopicConfig.TICKET_UPDATES_TOPIC,
+                0,
+                0L,
+                UUIDConverter.uuidToBytes(ticketId),
+                event
+        );
 
         // Act & Assert - Use StepVerifier to test reactive error handling
-        StepVerifier.create(ticketEventConsumer.handleTicketStatusUpdated(event, "ticket-updates.v1", 0, 0))
+        StepVerifier.create(ticketEventConsumer.handleTicketStatusUpdated(record))
                 .expectErrorMatches(throwable ->
                         throwable instanceof EntityNotFoundException &&
                                 throwable.getMessage().equals("Ticket not found: " + ticketId))
