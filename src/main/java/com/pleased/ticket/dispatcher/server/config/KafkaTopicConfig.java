953abbe10229cdb8930cdcfdc6a0813210997b81
Gentian Licenji <gentian.licenji@gmail.com>
2025-07-07 04:43:29 +0200

    PLS-3: Kafka Consumer Avro changes Refactored consumer config to 3 reactive consumers. Removed transactional mode since there is no need for a simple db insert. Changed keys from String to bytebuffer for avro adaptability. Refactored consumer service to adapt to the avro de-serialization and added consumer subscribers. Added avro schema registration to the KafkaTopicConfig.

--- src/main/java/com/pleased/ticket/dispatcher/server/config/KafkaTopicConfig.java
+++ src/main/java/com/pleased/ticket/dispatcher/server/config/KafkaTopicConfig.java
@@ -1,19 +1,44 @@
 package com.pleased.ticket.dispatcher.server.config;
 
+import com.pleased.ticket.dispatcher.server.model.events.TicketAssigned;
+import com.pleased.ticket.dispatcher.server.model.events.TicketCreated;
+import com.pleased.ticket.dispatcher.server.model.events.TicketStatusUpdated;
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import org.apache.avro.Schema;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.config.TopicConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @Profile("!embedded-kafka") // active when NOT in test
 @Configuration
 public class KafkaTopicConfig {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaTopicConfig.class);
 
     public static final String TICKET_CREATE_TOPIC = "ticket-create.v1";
     public static final String TICKET_ASSIGNMENTS_TOPIC = "ticket-assignments.v1";
     public static final String TICKET_UPDATES_TOPIC = "ticket-updates.v1";
+
+    public static final Map<String, String> EVENT_TYPE_MAP;
+    static {
+        Map<String, String> temp = new HashMap<>();
+        temp.put(TICKET_CREATE_TOPIC, "TicketCreated");
+        temp.put(TICKET_ASSIGNMENTS_TOPIC, "TicketAssigned");
+        temp.put(TICKET_UPDATES_TOPIC, "TicketStatusUpdated");
+        EVENT_TYPE_MAP = Collections.unmodifiableMap(temp);
+    }
 
     @Bean
     public NewTopic ticketCreateTopic() {
@@ -43,5 +68,22 @@ public class KafkaTopicConfig {
                 .config(TopicConfig.COMPRESSION_TYPE_CONFIG, "lz4")
                 .config(TopicConfig.SEGMENT_MS_CONFIG, "600000")
                 .build();
+    }
+
+    /**
+     * Avro Schema registration
+     */
+    private final SchemaRegistryClient client = new CachedSchemaRegistryClient("http://localhost:8081", 100);
+
+    @PostConstruct
+    public void registerSchemas() throws IOException, RestClientException {
+        register(TICKET_CREATE_TOPIC + "-value", TicketCreated.SCHEMA$);
+        register(TICKET_ASSIGNMENTS_TOPIC + "-value", TicketAssigned.SCHEMA$);
+        register(TICKET_UPDATES_TOPIC + "-value", TicketStatusUpdated.SCHEMA$);
+    }
+
+    private void register(String subject, Schema schema) throws IOException, RestClientException {
+        int id = client.register(subject, schema);
+        logger.info("Registered schema under subject={} with id={}", subject, id);
     }
 }
